04f7c18d5b37e3c1c95c449c0b2569d17cccf172
Christoph Herrmann <developer@dragonprojects.de>
2015-04-23 15:11:24 +0200

    Support mget to load multiple asset_ids of an user

--- modules/resume.js
+++ modules/resume.js
@@ -31,9 +31,17 @@ module.exports = function (config, libraries, services) {
         });
     });
 
-    app.get('/load/:user_id/:asset_id', function (req, res) {
-        client.get(req.params.user_id + ':' + req.params.asset_id, function (err, data) {
-            res.send(data);
-        });
+    app.get('/load/:user_id/:query', function (req, res) {
+        var query = JSON.parse(req.params.query);
+        if (typeof query == 'object') {
+            query = query.map(function (asset_id) { return req.params.user_id + ':' + asset_id; });
+            client.mget(query, function (err, data) {
+                res.send(data);
+            });
+        } else {
+            client.get(req.params.user_id + ':' + query, function (err, data) {
+                res.send(data);
+            });
+        }
     });
 };
